e7cc3ab037ff6f48856c5b54e5e30b080ce2f0bb
RamiB1234 <rbisthebest@hotmail.com>
2019-11-22 06:09:31 +0300

    Work on the quiz component design

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,12 +1,25 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import FlipCard from 'react-native-flip-card'
 
 
-class Quiz extends Component{
-    render(){
-        return(
+class Quiz extends Component {
+    render() {
+        return (
             <View style={styles.center}>
-                <Text style={{fontSize: 30, textAlign: 'center'}}>Quiz</Text>
+                <Text style={{ fontSize: 30, textAlign: 'center' }}>Quiz</Text>
+                <FlipCard>
+                    {/* Face Side */}
+                    <View style={styles.face}>
+                        <Text>Question</Text>
+                        <Text>bla bla bla bla bla bla bla bla bla?</Text>
+                    </View>
+                    {/* Back Side */}
+                    <View style={styles.back}>
+                        <Text>Answer</Text>
+                        <Text>bla bla bla bla bla bla :D</Text>
+                    </View>
+                </FlipCard>
                 <View style={styles.row}>
                     <TouchableOpacity
                         style={styles.submitButton}>
@@ -17,6 +30,14 @@ class Quiz extends Component{
                         <Text style={styles.submitButtonText}>Incorrect</Text>
                     </TouchableOpacity>
                 </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Text style={{ fontSize: 20 }}>
+                        Question 2/3
+                    </Text>
+                    <Text style={{ fontSize: 10 }}>
+                        Tab on the card to flip
+                    </Text>
+                </View>
             </View>
         )
     }
@@ -25,34 +46,43 @@ class Quiz extends Component{
 const styles = StyleSheet.create({
     center: {
         flex: 1,
-        justifyContent: 'flex-start',
+        justifyContent: 'space-between',
         marginLeft: 30,
         marginRight: 30,
-        marginTop:50,
-        marginBottom: 50
-      },
-      row: {
+        marginTop: 50,
+        marginBottom: 20
+    },
+    row: {
         flexDirection: 'row',
         flex: 1,
         alignItems: 'center'
-      },
-      submitButton: {
+    },
+    submitButton: {
+        flex: 1,
         backgroundColor: 'purple',
         padding: 10,
         paddingLeft: 30,
         paddingRight: 30,
-        height:45,
-        borderRadius:10,
+        height: 45,
+        borderRadius: 10,
         borderWidth: 1,
         borderColor: '#fff',
         justifyContent: 'center',
         alignItems: 'center'
-      },
-      submitButtonText: {
+    },
+    submitButtonText: {
         color: 'white',
         fontSize: 22,
         textAlign: 'center'
-      },
+    },
+    face: {
+        backgroundColor: 'yellow',
+        flex: 1
+    },
+    back:{
+        backgroundColor: 'red',
+        flex:1
+    }
 
 })
 
